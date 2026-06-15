e32047e2f26effe8c9af079339a46775b9e0e25e
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-06-15 12:13:38 +0530

    emptiness is checked

--- utils/RepositoryUtil.java
+++ utils/RepositoryUtil.java
@@ -7,8 +7,13 @@ import repository.IRepository;
 public class RepositoryUtil {
 
     public static <T> void printRepo(IRepository<T> repo) {
-        Iterator<T> iterator = repo.getAll().iterator();
+        if (repo.getAll().isEmpty()) {
+            System.out.println("No records found.");
+            return;
+        }
 
+        Iterator<T> iterator = repo.getAll().iterator();
+        
         while (iterator.hasNext()) {
             T item = iterator.next();
             System.out.println(item);
